5a6f31b0c83ba55ec76e516a2297c2d1bf1b48de
chandramani <cmmaurya9650@gmail.com>
2023-10-23 21:52:22 +0530

    code updated

---
+++ test.js
@@ -0,0 +1,42 @@
+// hamesh keval Arrayko hi sort kiya ja sakta hai if we want ot sort so first need to convert into array then sort then join
+// let s = "abdegrhsftef"
+// let ar = s.split("").sort().join(" ")
+
+// let d = {a:23,b:12,c:12,d:44}
+// if('a' in d){
+//     console.log(d['a'])
+// }else{
+//     console.log("nhi hai")
+// }
+
+// if(d.hasOwnProperty('r')){
+//     console.log("ha bhai hai")
+// }else{
+//     console.log("nhi hai re")
+// }
+
+// if(Object.keys(d).includes('a')){
+//    console.log("ha bhai hai re ")
+// }
+
+// if(s.includes('z')){
+//     console.log("hello world")
+// }
+num = 5
+for(let i =1;i<=num;i++){
+    let s= ""
+    let bag = "-"
+    for(let j=1;j<=2*num-2*i+1;j++){
+        if(j%2===1){
+            s+="*"
+       }else{
+           s+=bag
+       }
+    
+    
+}
+for(var k=0;k<2*i-2;k++){
+    s+=bag
+}
+console.log(s)
+}
